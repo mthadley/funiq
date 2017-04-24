83a2682c9b1df0d31976c3c9a428a5fb993417ff
Michael Hadley <mikethadley@gmail.com>
2017-04-24 13:28:58 -0700

    File hashing in parallel

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+extern crate futures;
+extern crate futures_cpupool;
+
+use futures::Future;
+use futures::future::join_all;
+use futures_cpupool::CpuPool;
 use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
 use std::fmt;
@@ -11,20 +17,33 @@ pub struct Error {
     path: Option<String>,
 }
 
-#[inline]
+/// Sets up a thread pool for parallel processing of files.
 pub fn process_files<'a>(paths: &'a [String]) -> Result<(Vec<&'a str>, Vec<&'a str>), Error> {
+    let pool = CpuPool::new(4);
+
+    let futures = paths.iter().cloned().map(|path| {
+        pool.spawn_fn::<_, Result<_, ()>>(move || {
+            let result = File::open(&path)
+                .and_then(hash_file)
+                .map_err(|e| {
+                    Error {
+                        inner: e,
+                        path: Some(path),
+                    }
+                });
+
+            Ok(result)
+        })
+    });
+
     let mut unique: HashMap<u64, &str> = HashMap::new();
     let mut duplicate: Vec<&str> = Vec::new();
 
-    for path in paths {
-        let hash = File::open(&path).and_then(hash_file)
-            .map_err(|e| {
-                Error {
-                    inner: e,
-                    path: Some(path.to_owned()),
-                }
-            })?;
+    let mut results = join_all(futures).wait()
+        .map_err(|_| io::Error::new(io::ErrorKind::Other, "Unknown Error."))?;
 
+    for (result, path) in results.drain(..).zip(paths) {
+        let hash = result?;
         if unique.contains_key(&hash) {
             duplicate.push(path);
         } else {
