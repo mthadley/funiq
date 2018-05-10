58d527887484a56795182243e4f8dc94b041aa19
Michael Hadley <mikethadley@gmail.com>
2018-05-10 18:04:48 +0000

    main can now return a Result in 1.26

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 extern crate clap;
 extern crate funiq;
 
-use std::process::exit;
-
 mod cli;
 
-fn main() {
-    cli::run().unwrap_or_else(|e| {
-        println!("{}", e);
-        exit(1);
-    });
+fn main() -> Result<(), funiq::Error> {
+    cli::run()
 }
