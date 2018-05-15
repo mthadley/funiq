7f4aaa2a287adeb4f5dff45353aacbfedfc405df
Michael Hadley <mikethadley@gmail.com>
2018-05-15 05:00:13 +0000

    Switch to rayon

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,12 @@
-extern crate futures;
-extern crate futures_cpupool;
-extern crate num_cpus;
+extern crate rayon;
 
-use futures::Future;
-use futures::future::join_all;
-use futures_cpupool::CpuPool;
-use std::collections::hash_map::DefaultHasher;
+use rayon::prelude::*;
 use std::collections::HashSet;
+use std::collections::hash_map::DefaultHasher;
 use std::fmt;
 use std::fs::File;
 use std::hash::{Hash, Hasher};
 use std::io::{self, Read};
-use std::env;
 
 #[derive(Debug)]
 pub struct Error {
@@ -19,40 +14,22 @@ pub struct Error {
     path: Option<String>,
 }
 
-/// Sets up a thread pool for parallel processing of files.
+/// Process files in parallel.
 pub fn process_files(paths: Vec<String>) -> Result<(Vec<String>, Vec<String>), Error> {
-    let pool = CpuPool::new(get_num_cpus());
-
-    let futures = paths.clone().into_iter().map(|path| {
-        pool.spawn_fn::<_, Result<_, ()>>(|| {
-            let result = File::open(&path)
-                .and_then(hash_file)
-                .map_err(|e| {
-                    Error {
-                        inner: e,
-                        path: Some(path),
-                    }
-                });
-
-            Ok(result)
-        })
-    });
-
-    let hashes = join_all(futures)
-        .wait()
-        .map_err(|_| io::Error::new(io::ErrorKind::Other, "Unknown Error."))?
-        .into_iter()
-        .collect::<Result<Vec<_>, _>>()?;
-
     let mut seen: HashSet<u64> = HashSet::new();
-    let (duplicate, unique) = hashes
+
+    let (duplicate, unique) = paths
+        .par_iter()
+        .map(|path| {
+            File::open(&path).and_then(hash_file).map_err(|e| Error {
+                inner: e,
+                path: Some(path.clone()),
+            })
+        })
+        .collect::<Result<Vec<_>, _>>()?
         .into_iter()
         .zip(paths)
-        .partition(|&(hash, _)| {
-            let is_duplicate = seen.contains(&hash);
-            seen.insert(hash);
-            is_duplicate
-        });
+        .partition(|&(hash, _)| !seen.insert(hash));
 
     Ok((map_snd(unique), map_snd(duplicate)))
 }
@@ -63,13 +40,6 @@ fn hash_file(file: File) -> io::Result<u64> {
         b?.hash(&mut hasher);
     }
     Ok(hasher.finish())
-}
-
-fn get_num_cpus() -> usize {
-    env::var("FUNIQ_NUM_CPU")
-        .ok()
-        .and_then(|val| val.parse().ok())
-        .unwrap_or_else(num_cpus::get)
 }
 
 fn map_snd<T, U>(pairs: Vec<(T, U)>) -> Vec<U> {
